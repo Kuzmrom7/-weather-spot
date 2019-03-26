cb420d25122a81e26613d4ac55ea697865f85c6d
Kuzmrom7 <kuzmrom7@gmail.com>
2019-03-26 19:31:12 +0300

    Updates

---
+++ precache-manifest.c321b444b189730f530cd658d212e753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58e2c6afbd2abc25136b",
+    "url": "/weather-spot/static/css/main.a78c83e0.chunk.css"
+  },
+  {
+    "revision": "58e2c6afbd2abc25136b",
+    "url": "/weather-spot/static/js/main.0102421c.chunk.js"
+  },
+  {
+    "revision": "5384b6dbf988ae38dc1f",
+    "url": "/weather-spot/static/js/runtime~main.f35108ba.js"
+  },
+  {
+    "revision": "8e82680f3ae38574ebc1",
+    "url": "/weather-spot/static/js/2.e15f74db.chunk.js"
+  },
+  {
+    "revision": "a1a0f20f045cd8461af13f6a741c490e",
+    "url": "/weather-spot/index.html"
+  }
+];
